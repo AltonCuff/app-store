22ab5aa025e66c2dfc05fb5f22f4843d297c6d25
Alton Cuff <bravadoinferno@gmail.com>
2020-11-18 16:57:53 +0200

    v2

--- client/src/components/mainpages/cart/PaypalButton.js
+++ client/src/components/mainpages/cart/PaypalButton.js
@@ -4,9 +4,7 @@ import PaypalExpressBtn from 'react-paypal-express-checkout';
 export default class PaypalButton extends React.Component {
     render() {
         const onSuccess = (payment) => {
-            // Congratulation, it came here means everything's fine!
             		console.log("The payment was succeeded!", payment);
-                    // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
                     this.props.tranSuccess(payment)
         }
  
@@ -20,7 +18,7 @@ export default class PaypalButton extends React.Component {
             // The main Paypal's script cannot be loaded or somethings block the loading of that script!
             console.log("Error!", err);
             // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
-            // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+           
         }
  
         let env = 'sandbox'; // you can set here to 'production' for production
@@ -38,7 +36,6 @@ export default class PaypalButton extends React.Component {
         // For production app-ID:
         //   => https://developer.paypal.com/docs/classic/lifecycle/goingLive/
  
-        // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
         let style = {
             size: 'small',
             color: 'blue',
